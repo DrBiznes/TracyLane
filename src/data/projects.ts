b3e9931a157889b04bc6e4c746306597742ac938
Jam Femino <jamesfemino@gmail.com>
2025-01-11 01:32:39 -0800

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -35,7 +35,7 @@ export const projectsData: ProjectsData = {
       body: "During a car ride back to his place, an old friend from high school showed me a song he was working on. I immediately knew I could make it sound great.  He gave me the opportunity, emailed his tracks, and let me know what he was expecting out of the mix. With just my laptop I made it into what it is today.",
       credits: [
         {
-          role: "Producer",
+          role: "Artist",
           name: "Trav"
         },
         {
@@ -52,18 +52,27 @@ export const projectsData: ProjectsData = {
       }
     },
     {
-      id: "through-space-and-time",
-      title: "Through Space and Time",
-      description: "Music Production & Creative Direction",
-      body: "An immersive journey through sonic landscapes, this project combines traditional orchestral elements with cutting-edge electronic production.",
+      id: "five-six-thousand-times",
+      title: "FIVE, SIX THOUSAND TIMES",
+      description: "Mixing and Mastering",
+      body: "Of all the songs on Jack's album, this one I think is the best. Yes, I am biased because I got to mix some of it. That being said, I don't think I need to defend that opinion. One listen should be enough for it to speak for itself. I am honored that I was part of making such a good song.",
+      credits: [
+        {
+          role: "Artist",
+          name: "Jack Redmond"
+        },
+        {
+          role: "Mixing",
+          name: "Tracy Lane"
+        }
+      ],
       imageUrl: "/project/ThroughSpace.jpg",
-      imageAlt: "Through Space and Time Album Cover",
-      backgroundImage: "/project/ThroughSpaceBackground.jpg",
+      imageAlt: "FIVE, SIX THOUSAND TIMES Album Cover",
+      backgroundImage: "/project/tsatmixing.jpg",
       aspectRatio: "square",
       links: {
-        spotify: "https://spotify.com/album/...",
-        appleMusic: "https://music.apple.com/album/...",
-        youtube: "https://youtube.com/..."
+        spotify: "https://open.spotify.com/track/1HY1hr2tKtsyxvQqaONiSY?si=b6106aa5aa1f49d2",
+        appleMusic: "https://music.apple.com/us/album/five-six-thousand-times/1616785888?i=1616785893"
       }
     }
   ]
